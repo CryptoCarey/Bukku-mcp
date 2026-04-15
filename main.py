0375bd172ebfc28bbccfc56e28dbdb7ae12f8d9e
CryptoCarey <158458321+CryptoCarey@users.noreply.github.com>
2026-04-15 10:27:04 +0800

    Update main.py

--- main.py
+++ main.py
@@ -145,4 +145,4 @@ app = Starlette(
 if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 8000))
-    uvicorn.run(app, host="0.0.0.0", port=port)
+  uvicorn.run(app, host="0.0.0.0", port=port, proxy_headers=True, forwarded_allow_ips="*")
